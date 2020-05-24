2561916fdbeb31159718182a42196b3d809cdf8e
Gaoyinghao <18600408534@163.com>
2020-05-24 15:28:06 +0800

    first commit

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="pay">
+  <div id="home">
     <div class="background_top"></div>
     <div class="background_center"></div>
     <div class="background_bottom">
       <div class="infoBox">
         <van-swipe class="my-swipe" indicator-color="white" @change="onChange">
-          <van-swipe-item>
+          <van-swipe-item >
             <div class="box-item">
               <p class="iphoneNum">158****3456</p>
               <p class="text">对方已振铃</p>
@@ -16,7 +16,11 @@
                 :options="playerOptions"
               ></video-player>
             </div>
-          </van-swipe-item>
+            </van-swipe-item>
+             <div class="custom-indicator" slot="indicator">
+        
+              </div>
+          
           <van-swipe-item>
             <div class="box-item">
               <p class="iphoneNum">158****3456</p>
@@ -29,23 +33,53 @@
               ></video-player>
             </div>
           </van-swipe-item>
-         
+           <van-swipe-item>
+            <div class="box-item">
+              <p class="iphoneNum">158****3456</p>
+              <p class="text">对方已振铃</p>
+              <video-player
+                class="video-player vjs-custom-skin"
+                ref="videoPlayer2"
+                :playsinline="true"
+                :options="playerOptions"
+              ></video-player>
+            </div>
+             <div class="custom-indicator" slot="indicator">
+        
+              </div>
+          </van-swipe-item>
+          <van-swipe-item>
+            <div class="box-item">
+              <p class="iphoneNum">158****3456</p>
+              <p class="text">对方已振铃</p>
+              <video-player
+                class="video-player vjs-custom-skin"
+                ref="videoPlayer2"
+                :playsinline="true"
+                :options="playerOptions"
+              ></video-player>
+            </div>
+           
+          </van-swipe-item>
+
         </van-swipe>
       </div>
     </div>
 
     <div class="footer">
-      <!-- <div class="item">
-      	<el-button type="success" @click="viewNumber" round>查看号码</el-button>
+      <div class="item">
+      	<div class="view" @click="viewNumber" v-if="isView">查看号码</div>  
       </div>
       <div class="item">
-      	<el-button type="primary" @click="openNow" round>立即开通</el-button>
-      </div>-->
+      	<div class="open" @click="openNow" v-if="isOpen">立即开通</div>
+      </div>
       <div class="item">
-        <el-button type="warning" @click="continueOpen" round>继续开通</el-button>
+        <div class="continue" @click="continueOpen" v-if="isContinue" >继续开通</div>
       </div>
     </div>
+    
   </div>
+  
 </template>
 
 <script>
@@ -54,7 +88,12 @@ export default {
   name: "Video",
   data() {
     return {
-       userIp :"",
+       banner:["1","2","3"],
+       isView :false,
+       isOpen :false,
+       isContinue:false,
+       paystate:"",
+       userIp :'',
        userId:'',
        usertype:'',
        playerOptions: {
@@ -70,7 +109,7 @@ export default {
           {
             type: "video/mp4",
             type: "video/ogg",
-            src: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" //url地址
+            src: "assets/mp4/中国联通.mp4" //url地址
           }
         ],
         poster:
@@ -86,21 +125,38 @@ export default {
       }
     };
   },
- mounted(){
-    this.getIp()
-  },
+  created(){
+    this.getIp();
+  },
   methods:{
+   yiDong:function() {
+      var u = navigator.userAgent;
+      var ua = window.navigator.userAgent.toLowerCase();//微信
+      var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android終端
+      var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios終端
+     if(ua.match(/MicroMessenger/i) == 'micromessenger'){
+       this.usertype = "openid"
+       getUrlParam();
+       getCode ();
+       getOpenId();
+     } 
+     else{
+      this.usertype = "ip"
+     }
+    },
+   
+   //获取IP
     getIp () {
       let that = this,
        params = {
           }
       this.$http.fetch('TmpUser/getip')
         .then(res => {
-          console.log(res);
           this.userIp = res.data.ip;
           this.getId();
         })
     },
+  //获取id
     getId () {
       let that = this,
           params = {
@@ -109,25 +165,51 @@ export default {
           }
       this.$http.fetch('TmpUser/getTmpUserId',params)
         .then(res => {
-          console.log(res);
+           this.userId =res.data.tmp_uid
+           this.orderStatus();
         })
     },
+  //根据临时用户ID查询订单
+    orderStatus(){
+      let that = this,
+          params = {
+           tmp_uid:this.userId,
+          }
+      this.$http.fetch('Order/stateus',params)
+        .then(res => {
+          if(res.state == undefined){
+            this.isOpen=true;
+            this.isContinue =false;
+            this.isView =false;
+          }
+          else if(res.state == 0){
+            this.isOpen=false;
+            this.isContinue =true;
+            this.isView =false;
+          }
+          else {
+            this.isOpen=false;
+            this.isContinue =false;
+            this.isView =true;
+          }
+        }) 
+      },
      getUrlParam(name) {//封装方法
-        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-        var r = window.location.search.substr(1).match(reg); //匹配目标参数
+        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); 
+        var r = window.location.search.substr(1).match(reg); 
         if (r != null) return unescape(r[2]);
         return null; //返回参数值
-    },
-     getCode () { // 非静默授权，第一次有弹框
-            const code = GetUrlParam('code') // 截取路径中的code，如果没有就去微信授权，如果已经获取到了就直接传code给后台获取openId
-            const local = window.location.href
-            if (code == null || code === '') {
-                window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + window.APPID + '&redirect_uri=' + encodeURIComponent(local) + '&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect'
-            } else {
-                this.getOpenId(code) //把code传给后台获取用户信息
-            }
+       },
+      getCode () { // 非静默授权，第一次有弹框
+        const code = GetUrlParam('code') // 截取路径中的code，如果没有就去微信授权，如果已经获取到了就直接传code给后台获取openId
+        const local = window.location.href
+        if (code == null || code === '') {
+            window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=' + window.APPID + '&redirect_uri=' + encodeURIComponent(local) + '&response_type=code&scope=snsapi_userinfo&state=1#wechat_redirect'
+        } else {
+            this.getOpenId(code) //把code传给后台获取用户信息
+        }
         },
-        getOpenId (code) { // 通过code获取 openId等用户信息，/api/user/wechat/login 为后台接口
+      getOpenId (code) { // 通过code获取 openId等用户信息，/api/user/wechat/login 为后台接口
             let _this = this
             this.$http.post('/api/user/wechat/login', {code: code}).then((res) => {
                 let datas = res.data
@@ -138,18 +220,37 @@ export default {
                 console.log(error)
             })
         },
-    
-
-
-  //  页面滑动视频停止播放
-   onChange(index){
+     onTouchStart (e) {
+        // this.clickIndex = 0; // 为了兼容安卓部分情况而加，如果不需要可忽略
+        this.clickFlag = false;
+      },
+      // 用于判断滑动还是点击
+      onTouchMove (e) {
+        this.clickFlag = true;
+        // 为了兼容安卓部分情况而加的判断，如果不需要可忽略，若需要，注释上面一行代码，打开下面三行代码
+        // this.clickIndex = this.clickIndex + 1;
+        // if (this.clickIndex > 2) {
+        //   this.clickFlag = true;
+        // }
+      },
+      onTouchEnd (position) {
+        if (this.clickFlag) { // 滑动
+          // console.log('滑动');
+        } else { // 点击
+          // console.log('点击');
+          this.handleShowPic(position);
+        }
+      },
+   
+    //页面滑动视频停止播放
+    onChange(index){
      this.$refs.videoPlayer1.player.pause()
     },  
     viewNumber() {
       this.$router.push({ path: "/payment" });
     },
     openNow() {
-      this.$router.push({ path: "/" });
+     this.$router.push({ path: "/sign" });
     },
     continueOpen() {
       this.$router.push({ path: "/account" });
@@ -160,11 +261,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#pay {
-  box-sizing: border-box;
-  width: 18.75rem;
-  margin: 0 auto;
-  position: relative;
+#home {
+   box-sizing: border-box;
+   width: 18.75rem;
+   margin: 0 auto;
+   position: relative;
   .background_top {
     height: 25.8rem;
     width: 100%;
@@ -191,19 +292,35 @@ export default {
       overflow: hidden;
     }
   }
-
+  /deep/.van-swipe__indicators{
+    background: #29224e !important;
+  }
   .footer {
     width: 14rem;
     position: absolute;
     left: 50%;
     bottom: 2rem;
     transform: translate(-50%, 0);
-    button {
-      margin-top: 0.5rem;
-      width: 100%;
+    .open{
+      background: #FD3B44;
     }
-  }
-}
+    .view{
+      background: #FE772D;
+    }
+    .continue{
+      background: -webkit-linear-gradient(left, #FE772D 0%,#FD3B44 100%);
+    }
+    }
+    }
+ .footer .item div{
+   width: 100%;
+   height: 2rem;
+   line-height: 2rem;
+   font-size: 0.8rem;
+   color: #fff;
+   border-radius: 1rem;
+   text-align: center;
+ }
 .van-swipe-item {
   width: 100%;
   height: 25.75rem;
@@ -218,6 +335,19 @@ export default {
   border-radius: 2rem;
   position: relative;
 }
+/deep/.van-swipe__indicators{
+  position: absolute;
+  left: 50%;
+  bottom:-1rem;
+  transform: translate(-50%,0);
+  height:0.4rem;
+   width: 100%;
+}
+/deep/.van-swipe__indicator{
+   height:1rem;
+   width: 1rem;
+   background: #29224e;
+ }
 .van-swipe-item .box-item .iphoneNum {
   position: absolute;
   left: 50%;
@@ -252,6 +382,11 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  width:4rem;
+  height: 4rem;
+  border:none;
+  border-radius:2rem;
+  line-height: 4rem;
 }
 /deep/.vjs_video_3 {
   width: 14rem;
