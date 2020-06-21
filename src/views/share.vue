5d7d9a9845f558704046cdb3121f87da63cd9d11
张琪 <abcxyz824@qq.com>
2020-06-21 22:12:30 +0800

    邀请卡引导页面样式修改完成

--- src/views/share.vue
+++ src/views/share.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="share">
-  	<div class="img">
+    <div class="img">
       <div class="box">
         <div class="company">{{company}}</div>
         <div class="name">{{name}}</div>
@@ -12,14 +12,24 @@
       <div id="img" v-if="!isIOS">
         <div id="qrcode" ref="qrcode"></div>
       </div>
-  	</div>
-  	<!-- <div class="buttons">
+    </div>
+    <!-- <div class="buttons">
       <div round>请分享邀请卡给员工填写号码</div>
   		<div class="b" type="primary" round>请长按图片保存</div>
-  	</div> -->
-    <div class="guide" v-if="guide && isWx" @click="()=>{guide = false}">
-      <img src="../../static/icon/guide.png"/>
-      <div class="text">点击分享按钮，分享给员工</div>
+    </div>-->
+    <div class="guide" v-if="guide" @click="()=>{guide = false}">
+      <div class="guideInner" ref="guide"></div>
+      <div class="img1">
+        <img src="../../static/icon/箭头1.png" />
+        <div>点击分享按钮，分享给员工</div>
+      </div>
+      <div class="img2" ref="guideImg2">
+        <div>长按二维码保存</div>
+        <img src="../../static/icon/箭头2.png" />
+      </div>
+      <div class="img3">
+        <img src="../../static/icon/我知道了.png" />
+      </div>
     </div>
   </div>
 </template>
@@ -27,50 +37,48 @@
 <script>
 import QRCode from "qrcodejs2";
 import CONFIG from "../../config/index.js";
-import TOOL from '../tools';
-import bus from '../bus.js';
+import TOOL from "../tools";
+import bus from "../bus.js";
 
 const wx = require("weixin-js-sdk");
 
 export default {
-  name: 'share',
-  props: {
-    
-  },
+  name: "share",
+  props: {},
   created() {
     var u = navigator.userAgent;
-    var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1;
+    var isAndroid = u.indexOf("Android") > -1 || u.indexOf("Adr") > -1;
 
     if (isAndroid) {
       this.isIOS = false;
     }
-    this.getCompanyInfo()
+    this.getCompanyInfo();
 
-    this.isWx = TOOL.getFacility() == 'Weixin';
+    this.isWx = TOOL.getFacility() == "Weixin";
   },
   mounted() {
     this.$nextTick(() => {
       this.custom();
       this.qrcode();
     });
-    bus.$on('onShare', () => {
+    bus.$on("onShare", () => {
       // this.onShare()
-    })
+    });
   },
-  data () {
-  	return {
-  		code: '',
+  data() {
+    return {
+      code: "",
       isIOS: true,
-      name: '',
-      company: '',
+      name: "",
+      company: "",
       guide: true,
       isWx: false
-  	}
+    };
   },
   methods: {
     // 自定义分享配置
-    custom () {
-      console.log('share')
+    custom() {
+      console.log("share");
       let that = this;
       // let share = {
       //   imgUrl: '',
@@ -78,47 +86,57 @@ export default {
       //   desc: '我的邀请卡',
       //   link: CONFIG.SHARE + "/sign.html?company_pid=" + that.$root.company_pid
       // };
-      TOOL.setShare(that, CONFIG.SHARE + "/sign.html?company_pid=" + that.$root.company_pid)
-  	},
+      TOOL.setShare(
+        that,
+        CONFIG.SHARE + "/sign.html?company_pid=" + that.$root.company_pid
+      );
+    },
     // 生成二维码参数
-    qrcode () {
+    qrcode() {
       let that = this;
       let clientWidth = document.body.clientWidth,
-          width = 85;
+        width = 85;
 
       let qrcode = new QRCode("QRCodeNone", {
-          width: width, // 二维码宽度，单位像素
-          height: width, // 二维码高度，单位像素
-          text: CONFIG.SHARE + "/sign.html?company_pid=" + that.$root.company_pid
-        });
-      var myCanvas = document.getElementsByTagName('canvas')[0];
-      var qrcodeNode = document.getElementById('qrcode');
+        width: width, // 二维码宽度，单位像素
+        height: width, // 二维码高度，单位像素
+        text: CONFIG.SHARE + "/sign.html?company_pid=" + that.$root.company_pid
+      });
+      var myCanvas = document.getElementsByTagName("canvas")[0];
+      var qrcodeNode = document.getElementById("qrcode");
       var img = that.convertCanvasToImage(myCanvas);
       qrcodeNode.appendChild(img);
-      console.log('分享链接： ' + CONFIG.SHARE + "/sign.html?company_pid=" + this.$root.company_pid)
-    },
+      // console.log('分享链接： ' + CONFIG.SHARE + "/sign.html?company_pid=" + this.$root.company_pid)
+
+      let top = this.$refs.qrcode.getBoundingClientRect().top;
+      let left = this.$refs.qrcode.getBoundingClientRect().left;
+      this.$refs.guide.style.borderTopWidth = top - 10 + "px";
+      this.$refs.guide.style.borderLeftWidth = left - 8 + "px";
+      this.$refs.guideImg2.style.top = top - 80 + "px";
+    },
     //将canvas返回的图片添加到image里
-    convertCanvasToImage(canvas){
+    convertCanvasToImage(canvas) {
       var image = new Image();
       image.src = canvas.toDataURL("image/png");
       return image;
     },
     // 获取企业信息
-    getCompanyInfo () {
+    getCompanyInfo() {
       let that = this;
 
-      that.$http.fetch('Company/getCompanyInfo', {
-        company_id: that.$distributorId
-      })
-      .then(res => {
-        if (res.errNo == 0) {
-          that.company = res.data.company_name;
-          that.name = res.data.contact_name;
-        }
-      }) 
+      that.$http
+        .fetch("Company/getCompanyInfo", {
+          company_id: that.$distributorId
+        })
+        .then(res => {
+          if (res.errNo == 0) {
+            that.company = res.data.company_name;
+            that.name = res.data.contact_name;
+          }
+        });
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -130,19 +148,19 @@ export default {
   user-select: none;
 
   .text {
-  	text-align: center;
+    text-align: center;
     font-size: 0.9rem;
   }
   .img {
-  	position: relative;
-  	text-align: center;
-  	height: 8.15rem;
-  	width: 15.85rem;
+    position: relative;
+    text-align: center;
+    height: 8.15rem;
+    width: 15.85rem;
     max-width: 100%;
     margin: 0 auto;
     // margin-top: 1.45rem;
     // margin-bottom: 1.25rem;
-    background-image: url('../../static/icon/share_bg.png');
+    background-image: url("../../static/icon/share_bg.png");
     background-size: cover;
     top: 50%;
     transform: translateY(-50%);
@@ -179,7 +197,7 @@ export default {
       // width: 9.35rem;
       // text-align: left;
       margin-bottom: 0.5rem;
-      overflow : hidden;
+      overflow: hidden;
       text-overflow: ellipsis;
       display: -webkit-box;
       -webkit-line-clamp: 2;
@@ -191,12 +209,13 @@ export default {
       // left: 0.75rem;
     }
 
-  	#qrcode,#QRCodeNone {
-  		position: absolute;
+    #qrcode,
+    #QRCodeNone {
+      position: absolute;
       bottom: 0;
       left: 0;
       width: 100%;
-  	}
+    }
     #QRCodeNone {
       display: none;
     }
@@ -211,25 +230,25 @@ export default {
     }
   }
   .buttons {
-  	div {
-  		display: block;
-  		margin: auto;
-  		margin-bottom: 1rem;
+    div {
+      display: block;
+      margin: auto;
+      margin-bottom: 1rem;
       border: none;
       padding: 0.45rem 0rem;
-      background:rgba(64,173,194,1);
-      border-radius:0.2rem;
+      background: rgba(64, 173, 194, 1);
+      border-radius: 0.2rem;
       font-size: 0.8rem;
-      font-weight:400;
-      color:rgba(255,255,255,1);
+      font-weight: 400;
+      color: rgba(255, 255, 255, 1);
       line-height: 1.1rem;
-      margin-bottom: 0.8rem; 
+      margin-bottom: 0.8rem;
       text-align: center;
-  	}
+    }
     .b {
-      border: 0.05rem solid rgba(64,173,194,1);
+      border: 0.05rem solid rgba(64, 173, 194, 1);
       background: #fff;
-      color: #40ADC2;
+      color: #40adc2;
     }
   }
   .guide {
@@ -239,22 +258,65 @@ export default {
     right: 0;
     bottom: 0;
     z-index: 100000;
-    background:rgba(0,0,0,0.7);
-
-    img{
-      width: 144px;
-      height: 106px;
+    background: transparent;
+    .guideInner {
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      z-index: 0;
+      box-sizing: content-box;
+      width: 102px;
+      height: 102px;
+      border-color: rgba(0, 0, 0, 0.7);
+      border-style: solid;
+      border-width: 350px 1500px 1500px 238px;
+      background: transparent;
+    }
+    .img1 {
       position: absolute;
       top: 0.15rem;
-      right: 1.7rem;
+      right: 0;
+      left: 0;
+      text-align: right;
+      img {
+        width: 66px;
+        margin-right: 1.7rem;
+      }
+      div {
+        text-align: center;
+        font-size: 18px;
+        color: rgba(255, 255, 255, 1);
+        line-height: 20px;
+      }
     }
-    .text {
-      font-size:18px;
-      color:rgba(255,255,255,1);
-      line-height:20px;
+    .img2 {
       position: absolute;
-      top: 5.75rem;
-      right: 3.2rem;
+      right: 0;
+      left: 0;
+      text-align: right;
+      img {
+        width: 45px;
+        margin-right: 4.6rem;
+      }
+      div {
+        text-align: center;
+        font-size: 18px;
+        color: rgba(255, 255, 255, 1);
+        line-height: 20px;
+      }
+    }
+
+    .img3 {
+      position: absolute;
+      bottom: 1rem;
+      right: 0;
+      left: 0;
+      text-align: center;
+      img {
+        width: 166px;
+      }
     }
   }
 }
