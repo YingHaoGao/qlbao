2d7c12e8b1ee936621d3b8f5ebc50f2135c0e1cd
张琪 <abcxyz824@qq.com>
2020-07-02 22:33:46 +0800

    修改首页 添加营业执照等信息

--- src/views/home.vue
+++ src/views/home.vue
@@ -8,7 +8,7 @@
     :class="{full: size > 3.2}"
     v-loading="loading"
   >
-    <img style="width: 100%" src="../assets/img/background1.png" alt="">
+    <img style="width: 100%" src="../assets/img/background1.png" alt />
     <!-- <div class="background_top"></div> -->
     <!-- <div class="background_center"></div> -->
     <!-- <div class="background_bottom"> -->
@@ -56,6 +56,42 @@
       <div></div>
     </div>
     <!-- <iframe :src="src" frameborder="0" sandbox="allow-scripts allow-top-navigation allow-same-origin"></iframe> -->
+    <div class="info certificate">
+      <img src="../assets/img/证书.png" alt />
+      <div class="info-inner">
+        <van-swipe indicator-color="#1F71CE" :loop="true">
+          <van-swipe-item>
+            <img src="../assets/img/zhizhao.png" alt />
+          </van-swipe-item>
+          <van-swipe-item>
+            <img src="../assets/img/zhizhao2.png" alt />
+          </van-swipe-item>
+          <van-swipe-item>
+            <img src="../assets/img/zhizhao3.png" alt />
+          </van-swipe-item>
+          <van-swipe-item>
+            <img src="../assets/img/zhizhao4.png" alt />
+          </van-swipe-item>
+        </van-swipe>
+      </div>
+    </div>
+    <div class="info">
+      <img src="../assets/img/理由.png" alt />
+    </div>
+    <div class="info contact">
+      <img src="../assets/img/联系我们.png" alt />
+      <div class="info-inner">
+        <div>
+          <p>联系人：罗云</p>
+          <p>联系方式：15201203405</p>
+          <p>邮箱：luo_yun@newautogroup.cn</p>
+          <div class="qr-cord">
+            <img src="../assets/img/qr-cord.png" alt />
+            <div>更多问题请扫码联系我们</div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -63,7 +99,7 @@
 import TOOL from "../tools.js";
 import CONFIG from "../../config/index.js";
 import axios from "axios";
-import $ from 'jquery';
+import $ from "jquery";
 // import Swiper from "swiper"
 import Background from "../assets/img/background0.png";
 
@@ -96,7 +132,7 @@ export default {
           sources: [
             {
               type: "video/mp4",
-              src: require("../assets/mp4/贵州酒中酒集团.mp4")                 // 本地
+              src: require("../assets/mp4/贵州酒中酒集团.mp4") // 本地
             }
           ],
           // poster: "../../static/video.png", //你的封面地址
@@ -108,7 +144,8 @@ export default {
             remainingTimeDisplay: false,
             fullscreenToggle: true //全屏按钮
           }
-        },{
+        },
+        {
           playbackRates: false, //播放速度
           autoplay: false, //如果true,浏览器准备好时开始回放。
           muted: false, // 默认情况下将会消除任何音频。
@@ -120,8 +157,8 @@ export default {
           sources: [
             {
               type: "video/mp4",
-              src: require("../assets/mp4/江西竹可健竹汁饮料有限公司.mp4")                 // 本地
-            },
+              src: require("../assets/mp4/江西竹可健竹汁饮料有限公司.mp4") // 本地
+            }
           ],
           // poster: "../../static/video.png", //你的封面地址
           width: document.documentElement.clientWidth,
@@ -132,7 +169,8 @@ export default {
             remainingTimeDisplay: false,
             fullscreenToggle: true //全屏按钮
           }
-        },{
+        },
+        {
           playbackRates: false, //播放速度
           autoplay: false, //如果true,浏览器准备好时开始回放。
           muted: false, // 默认情况下将会消除任何音频。
@@ -144,31 +182,8 @@ export default {
           sources: [
             {
               type: "video/mp4",
-              src: require("../assets/mp4/燕之屋.mp4")                 // 本地
-            },
-          ],
-          width: document.documentElement.clientWidth,
-          notSupportedMessage: "此视频暂无法播放，请稍后再试", //允许覆盖Video.js无法播放媒体源时显示的默认信息。
-          controlBar: {
-            timeDivider: false,
-            durationDisplay: false,
-            remainingTimeDisplay: false,
-            fullscreenToggle: true //全屏按钮
-          }
-        },{
-          playbackRates: false, //播放速度
-          autoplay: false, //如果true,浏览器准备好时开始回放。
-          muted: false, // 默认情况下将会消除任何音频。
-          loop: false, // 导致视频一结束就重新开始。
-          preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
-          language: "zh-CN",
-          aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
-          fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
-          sources: [
-            {
-              type: "video/mp4",
-              src: require("../assets/mp4/分众传媒.mp4")                 // 本地
-            },
+              src: require("../assets/mp4/燕之屋.mp4") // 本地
+            }
           ],
           width: document.documentElement.clientWidth,
           notSupportedMessage: "此视频暂无法播放，请稍后再试", //允许覆盖Video.js无法播放媒体源时显示的默认信息。
@@ -179,6 +194,30 @@ export default {
             fullscreenToggle: true //全屏按钮
           }
         },
+        {
+          playbackRates: false, //播放速度
+          autoplay: false, //如果true,浏览器准备好时开始回放。
+          muted: false, // 默认情况下将会消除任何音频。
+          loop: false, // 导致视频一结束就重新开始。
+          preload: "auto", // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+          language: "zh-CN",
+          aspectRatio: "16:9", // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+          fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+          sources: [
+            {
+              type: "video/mp4",
+              src: require("../assets/mp4/分众传媒.mp4") // 本地
+            }
+          ],
+          width: document.documentElement.clientWidth,
+          notSupportedMessage: "此视频暂无法播放，请稍后再试", //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+          controlBar: {
+            timeDivider: false,
+            durationDisplay: false,
+            remainingTimeDisplay: false,
+            fullscreenToggle: true //全屏按钮
+          }
+        }
       ],
       load: false,
       size: false,
@@ -224,7 +263,7 @@ export default {
     let agent_id = this.getUrlKey("id");
     this.$root.agent_id = agent_id;
 
-    this.noBtn = this.getUrlKey('no_btn');
+    this.noBtn = this.getUrlKey("no_btn");
     if (this.noBtn) return;
 
     this.load = this.$loading({
@@ -369,7 +408,7 @@ export default {
           TOOL.alert("订单id = " + JSON.stringify(res.data));
 
           this.btnType = 2;
-          res.data.some((order) => {
+          res.data.some(order => {
             if (order.state == 0) {
               // 未支付
               this.btnType = 1;
@@ -437,32 +476,38 @@ export default {
     onChange(index) {
       this.seiperIndex = index;
       // console.log(this.$refs.videoPlayer1[0]);
-      if(this.seiperIndex === 1){
+      if (this.seiperIndex === 1) {
         this.$refs.videoPlayer1[0].player.pause();
         this.$refs.videoPlayer3[0].player.pause();
         this.$refs.videoPlayer4[0].player.pause();
-      }else if(this.seiperIndex === 2){
+      } else if (this.seiperIndex === 2) {
         this.$refs.videoPlayer1[0].player.pause();
         this.$refs.videoPlayer2[0].player.pause();
         this.$refs.videoPlayer4[0].player.pause();
-      }else if(this.seiperIndex === 3){
+      } else if (this.seiperIndex === 3) {
         this.$refs.videoPlayer3[0].player.pause();
         this.$refs.videoPlayer1[0].player.pause();
         this.$refs.videoPlayer2[0].player.pause();
-      }else if(this.seiperIndex === 0){
+      } else if (this.seiperIndex === 0) {
         this.$refs.videoPlayer2[0].player.pause();
         this.$refs.videoPlayer3[0].player.pause();
         this.$refs.videoPlayer4[0].player.pause();
       }
-      console.log(index, $('.van-swipe__track'))
-      if(index == 0) {
+      console.log(index, $(".van-swipe__track"));
+      if (index == 0) {
         setTimeout(() => {
-          let w1 = $('.van-swipe__track').width();
-          let w2 = $('.active-item-one').width();
-          console.log(w1, w2)
-          $('.van-swipe__track').attr('style', 'width:' + w1 + 'px;transform: translateX(0px)')
-          $('.active-item-one').attr('style', 'width:' + w2 + 'px;transform: translateX(0px)')
-        }, 300)
+          let w1 = $(".van-swipe__track").width();
+          let w2 = $(".active-item-one").width();
+          console.log(w1, w2);
+          $(".van-swipe__track").attr(
+            "style",
+            "width:" + w1 + "px;transform: translateX(0px)"
+          );
+          $(".active-item-one").attr(
+            "style",
+            "width:" + w2 + "px;transform: translateX(0px)"
+          );
+        }, 300);
       }
       // this.$refs.videoPlayer1.player.pause();
     },
@@ -479,7 +524,7 @@ export default {
       //  tmp_uid: that.tmp_uid,
       //   company_id: that.company_id
       // } });
-      window.location.href = `${CONFIG.SHARE}#/sign?company_id=${that.company_id}&tmp_uid=${that.tmp_uid}`
+      window.location.href = `${CONFIG.SHARE}#/sign?company_id=${that.company_id}&tmp_uid=${that.tmp_uid}`;
     },
     continueOpen() {
       let that = this;
@@ -487,7 +532,7 @@ export default {
       // this.$router.push({ path: window.location.href.split('?')[0] + "/account", query: {
       //    company_id: that.company_id
       // } });
-      window.location.href = `${CONFIG.SHARE}#/account?company_id=${that.company_id}&tmp_uid=${that.tmp_uid}`
+      window.location.href = `${CONFIG.SHARE}#/account?company_id=${that.company_id}&tmp_uid=${that.tmp_uid}`;
       // window.location.href = `${
       //   window.location.href.split("?")[0]
       // }#/account?company_id=${that.company_id}&tmp_uid=${that.tmp_uid}`;
@@ -500,11 +545,9 @@ export default {
 #home {
   box-sizing: border-box;
   width: 100%;
-  // height: 58.4rem;
   margin: 0 auto;
   position: relative;
   background-size: cover;
-  // background-position: center;
   top: 2rem;
   overflow: hidden;
 
@@ -519,7 +562,6 @@ export default {
   .background_top {
     height: 25.8rem;
     width: 100%;
-    // background: url(../assets/img/background_top.png) no-repeat center;
     background-size: cover;
   }
   .background_center {
@@ -527,37 +569,98 @@ export default {
     height: 4.35rem;
     width: 100%;
   }
-  // .background_bottom {
-  //   // background: url(../assets/img/background_botom.png) no-repeat center;
-  //   background-size: cover;
-  //   width: 100%;
-  //   height: 28.25rem;
-  //   position: relative;
-  //   .infoBox {
-  //     position: absolute;
-  //     top: 26.5rem;
-  //     left: 0;
-  //     right: 0;
-  //     height: 27rem;
-  //     overflow: hidden;
-  //   }
-  // }
+  .van-swipe-item {
+    float: left;
+  }
 
-  .infoBox{
+  /deep/.van-swipe__indicators {
+    width: 100%;
+    text-align: center;
+  }
+  /deep/.van-swipe__indicator {
+    display: inline-block;
+    border-radius: 50%;
+    &:last-child {
+      margin-right: 0 !important;
+    }
+  }
+
+  .infoBox {
     padding-top: 20px;
+
+    .van-swipe-item .box-item {
+      width: 14.4rem;
+      height: 25.75rem;
+      background: url(../assets/img/phone.png) no-repeat center;
+      background-size: cover;
+      border-radius: 2rem;
+      position: relative;
+    }
+    /deep/.van-swipe__indicators {
+      width: 100%;
+      text-align: center;
+    }
+    /deep/.van-swipe__indicator {
+      height: 8px;
+      width: 8px;
+      background: #ffffff;
+      margin-right: 10px;
+    }
+    .van-swipe-item .box-item .iphoneNum {
+      position: absolute;
+      left: 50%;
+      top: 2.5rem;
+      transform: translate(-50%);
+      color: #ffff;
+      font-size: 1.1rem;
+    }
+
+    .van-swipe-item .box-item .text {
+      position: absolute;
+      top: 4.5rem;
+      left: 50%;
+      transform: translate(-50%);
+      color: #cacaca;
+      font-size: 0.75rem;
+    }
+    .van-swipe-item .box-item .video-player {
+      position: absolute;
+      top: 7.7rem;
+      left: 0.2rem;
+      width: 14rem;
+      height: 8rem;
+      margin: 0 auto;
+      .vjs_video_3 {
+        width: 14rem;
+        height: 7.9rem;
+      }
+    }
+    /deep/.video-js .vjs-big-play-button {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 4rem;
+      height: 4rem;
+      border: none;
+      border-radius: 2rem;
+      line-height: 4rem;
+    }
+    /deep/.vjs_video_3 {
+      width: 14rem;
+      height: 7.9rem;
+    }
+    /deep/.vjs-custom-skin > .video-js {
+      width: 14rem;
+      height: 8rem;
+    }
   }
-  .van-swipe__indicators {
-    background: #29224e !important;
-  }
+
   .footer {
     width: 14rem;
     margin: auto;
     padding-top: 20px;
     padding-bottom: 20px;
-    // position: absolute;
-    // left: 50%;
-    // bottom: 1.5rem;
-    // transform: translate(-50%, 0);
     .open {
       background: linear-gradient(
         360deg,
@@ -585,95 +688,73 @@ export default {
       font-weight: bold;
       letter-spacing: 4px;
     }
+    .item div {
+      width: 100%;
+      height: 2rem;
+      line-height: 2rem;
+      font-size: 0.9rem;
+      color: #fff;
+      border-radius: 1rem;
+      text-align: center;
+    }
   }
-}
-.footer .item div {
-  width: 100%;
-  height: 2rem;
-  line-height: 2rem;
-  font-size: 0.9rem;
-  color: #fff;
-  border-radius: 1rem;
-  text-align: center;
-}
-.van-swipe-item {
-  width: 100%;
-  height: 25.75rem;
-  float: left;
-}
-.van-swipe-item .box-item {
-  width: 14.4rem;
-  height: 25.75rem;
-  background: url(../assets/img/phone.png) no-repeat center;
-  background-size: cover;
-  border-radius: 2rem;
-  position: relative;
-}
-/deep/.van-swipe__indicators {
-  width: 100%;
-  text-align: center;
-}
-/deep/.van-swipe__indicator {
-  display: inline-block;
-  height: 8px;
-  width: 8px;
-  background: #ffffff;
-  border-radius: 50%;
-  margin-right: 10px;
-  &:last-child {
-    margin-right: 0;
-  }
-}
-.van-swipe-item .box-item .iphoneNum {
-  position: absolute;
-  left: 50%;
-  top: 2.5rem;
-  transform: translate(-50%);
-  color: #ffff;
-  font-size: 1.1rem;
-}
 
-.van-swipe-item .box-item .text {
-  position: absolute;
-  top: 4.5rem;
-  left: 50%;
-  transform: translate(-50%);
-  color: #cacaca;
-  font-size: 0.75rem;
-}
-.van-swipe-item .box-item .video-player {
-  position: absolute;
-  top: 7.7rem;
-  left: 0.2rem;
-  width: 14rem;
-  height: 8rem;
-  margin: 0 auto;
-  .vjs_video_3 {
-    width: 14rem;
-    height: 7.9rem;
+  .info {
+    padding: 10px;
+    position: relative;
+    > img {
+      width: 100%;
+    }
+    .info-inner {
+      position: absolute;
+      top: 10px;
+      left: 10px;
+      right: 10px;
+      bottom: 10px;
+    }
   }
-}
-/deep/.video-js .vjs-big-play-button {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 4rem;
-  height: 4rem;
-  border: none;
-  border-radius: 2rem;
-  line-height: 4rem;
-}
-/deep/.vjs_video_3 {
-  width: 14rem;
-  height: 7.9rem;
-}
-/deep/.vjs-custom-skin > .video-js {
-  width: 14rem;
-  height: 8rem;
-}
+  .certificate {
+    .info-inner {
+      overflow: hidden;
+      .van-swipe {
+        text-align: center;
+        margin-top: 30%;
+      }
+      img {
+        width: 90%;
+      }
+    }
+    /deep/.van-swipe__indicator {
+      height: 12px;
+      width: 12px;
+      background: #79d4e4;
+      margin-right: 30px;
+    }
+  }
 
-.my-swipe {
-  height: 100%;
+  .contact {
+    color: #333;
+    font-size: 15px;
+    font-weight: bold;
+    .info-inner {
+      > div {
+        margin-top: 12%;
+        > p {
+          margin-left: 12%;
+          margin-bottom: 1%;
+        }
+        .qr-cord {
+          margin-top: 6%;
+          text-align: center;
+          img {
+            width: 104px;
+          }
+          div {
+            font-size: 12px;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
