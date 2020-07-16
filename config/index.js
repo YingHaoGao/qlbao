df743305b144875f47c6c3db38e19cb194d2d5d7
btsd5 <chenrui>
2020-07-16 18:31:24 +0800

    fix bugs.

--- config/index.js
+++ config/index.js
@@ -1,14 +1,14 @@
-var HTTP = 'http://testapi.cailing.meisheapp.com/',
+var HTTP = 'http://api.cailing.meisheapp.com/',
 		WXHTTP = '/meishe-wxapi',
 		ROOT = location.origin + location.pathname,
 		SHARE_LINK = "http://m.meisheapp.com/videoRinging/?name=demo";
 
 // 服务器地址
 // if (process.env.NODE_ENV === "development") {
-//   HTTP = 'http://testapi.cailing.meisheapp.com/';
+//   HTTP = 'http://api.cailing.meisheapp.com/';
 // 	WXHTTP = '/meishe-wxapi';
 // }else {
-//   HTTP = 'http://testapi.cailing.meisheapp.com/';
+//   HTTP = 'http://api.cailing.meisheapp.com/';
 // 	WXHTTP = '/meishe-wxapi';
 // }
 
